e0da9f8ab2ad910b5e38af323aedbac5fcd96106
Matt Gostick <matt@gostick.ca>
2019-01-15 14:48:18 -0500

    actually adding terminator config

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cp .screenrc $HOME/
 cp .vimrc $HOME/
 cp -Rp .vim $HOME/
+cp -Rp .config/terminator $HOME/.config/
 
 cp .dircolors $HOME/
 echo 'eval $(dircolors -b $HOME/.dircolors)' >> $HOME/.bashrc
